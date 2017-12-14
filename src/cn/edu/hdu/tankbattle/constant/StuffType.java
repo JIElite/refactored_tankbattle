baea9fe1f0c12dcacf437ee6c7a0dbbdbc336b3d
JIElite <ita3051@gmail.com>
2017-12-14 22:11:16 +0800

    delete useless comment

--- src/cn/edu/hdu/tankbattle/constant/StuffType.java
+++ src/cn/edu/hdu/tankbattle/constant/StuffType.java
@@ -1,20 +1,8 @@
 package cn.edu.hdu.tankbattle.constant;
 
 public class StuffType {
-	/**
-	 * 砖块
-	 */
 	public static final int BRICK = 0;
-	/**
-	 * 铁块
-	 */
 	public static final int IRON = 1;
-	/**
-	 * 水池
-	 */
 	public static final int WATER = 2;
-	/**
-	 * 坦克
-	 */
 	public static final int TANK = 3;
 }
